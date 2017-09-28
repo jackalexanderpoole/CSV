9f02df98508d5f4c2dc507f2bea9134743754d96
jackalexanderpoole <32180221+jackalexanderpoole@users.noreply.github.com>
2017-09-28 19:51:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import csv
+import os
 
 ACCEPTED_MSG = """
 Dear {},
-
 You have made it onto our course!
 Your coach will be {}.
-
 Thank you
 """
 
 REJECTED_MSG = """
 Dear {}
-
 I'm afraid you haven't been accepted for our course.
-
 Thank you
 """
+
+file_path = 'text.txt'
+
+
+file_exists = os.path.exists(file_path)
+if file_exists:
+	print('Text file exists')
+else:
+	print('Unable to find text file')
+	
+file = open(file_path, 'r+')
+
+for line in file:
+	print(line)
 
 with open('sheet.csv') as csv_file:
 
@@ -27,11 +38,11 @@ with open('sheet.csv') as csv_file:
 
         if accepted == "yes":
             msg = ACCEPTED_MSG.format(name,coach)
+            file.write(msg)
         else:
             msg = REJECTED_MSG.format(name)
+            file.write(msg)
 
         print("Send email to: {}".format(email))
         print("Email content: ")
         print(msg)
-
-
